890baeaf5a2973606504f20508c55dd50848f53e
JaimeCAST69 <20223tn088@utez.edu.mx>
2024-04-11 08:01:16 -0600

    Update Login.jsx

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     try {
       const payload = { user: user, password: password };
       console.log(payload);
-      const response = await fetch("http://192.168.1.70:8080/api/auth/signin", {
+      const response = await fetch("http://192.168.0.53:8080/api/auth/signin", {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
 
@@ -126,78 +126,149 @@ const Login = () => {
         }
       </View>
     </ImageBackground>
+  );return (
+  
+    <ImageBackground
+      source={require("../assets/fondo.png")}
+      style={styles.backgroundImage}
+    >
+
+
+      <View
+       
+        style={{ flex: 1, flexDirection: "row", marginTop: '10%' , justifyContent: 'space-around'}}
+      >
+        <Text style={styles.titleGastromanager}>Gastromanager</Text>
+        <Image
+          source={require("../assets/gastromanager2.png")}
+          style={styles.logo}
+        />
+      </View>
+
+
+      <View style={styles.container}>
+        {
+          <View style={styles.container}>
+            <View style={styles.formContainer}>
+              <Text style={styles.titleLogin}>Bienvenido</Text>
+              <Text style={styles.TextLogin}>
+                Inicie sesión para poder acceder a las funciones del sistema
+              </Text>
+              <View style={styles.inputContainer}>
+                <Icon name="user" size={20} color="green" style={styles.icon} />
+                <TextInput
+                  placeholder="User"
+                  placeholderTextColor={'white'}
+                  value={user}
+                  onChangeText={setUser}
+                  style={styles.inputForm}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Icon name="lock" size={20} color="green" style={styles.icon} />
+                <TextInput
+                  placeholder="Password"
+                  placeholderTextColor={'white'}
+                  value={password}
+                  onChangeText={setPassword}
+                  style={styles.inputForm}
+                  secureTextEntry
+                />
+              </View>
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={checkUser}
+              >
+                <Text style={styles.buttonText}>Iniciar Sesión</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        }
+      </View>
+    </ImageBackground>
   );
 }
-
 
 const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: "rgba(9,43,90,100)",
-    padding: hp('2%'),
-    borderRadius: hp('1.5%'),
-    marginTop: hp('10%'),
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 80,
   },
+
   buttonText: {
     color: "white",
     textAlign: "center",
     fontWeight: "bold",
   },
+
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: wp('5%'),
+    padding: 20,
+    
   },
+
   backgroundImage: {
     flex: 1,
     resizeMode: "cover",
   },
+
   inputContainer: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: hp('2.5%'),
+    marginBottom: 20,
   },
+
   icon: {
-    marginRight: wp('2%'),
+    marginRight: 10,
   },
+
   inputForm: {
     flex: 1,
     borderBottomColor: "rgba(255, 255, 255, 100)",
-    borderBottomWidth: wp('0.5%'),
-    borderRadius: wp('1%'),
-    padding: wp('2%'),
-    marginVertical: hp('0.5%'),
+    borderBottomWidth: 3,
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
     color: "rgba(255, 255, 255, 100)",
   },
+
   formContainer: {
-    marginTop: -hp('45%'),
-    padding: wp('2%'),
+    marginTop: -390,
+    padding: 10,
     backgroundColor: "rgba(0, 0, 0, 0.3)",
-    borderRadius: wp('5%'),
-    height: hp('65%'),
+    borderRadius: 20,
+    height: 500,
     justifyContent: "center",
   },
+
   titleLogin: {
     color: "rgba(120, 168, 144, 100)",
-    fontSize: wp('13%'),
+    fontSize: 50,
     fontWeight: "bold",
   },
+
   TextLogin: {
     color: "rgba(120, 168, 144, 100)",
-    fontSize: wp('5%'),
-    marginBottom: hp('5%'),
-    marginTop: hp('2.5%'),
+    fontSize: 20,
+    marginBottom: 40,
+    marginTop: 20,
   },
+
   logo: {
-    width: wp('18%'),
-    height: hp('8%'),
-    marginRight: wp('2%'),
-    marginStart: wp('3%'),
+    width: 60,
+    height: 60,
+    marginRight: 10,
+    marginStart: 30,
   },
+
   titleGastromanager: {
     color: "rgba(255, 255, 255, 100)",
-    fontSize: wp('9%'),
+    fontSize: 35,
     fontWeight: "bold",
-    marginTop: hp('0.5%'),
+    marginTop: 5,
   },
 });
 
